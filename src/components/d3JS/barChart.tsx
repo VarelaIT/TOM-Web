0b17637e81a2c30608cb825b65939b3a5f613d4b
Ismael Varela <info@varelait.com>
2024-11-10 23:38:12 -0400

    bar chart completed

--- src/components/d3JS/barChart.tsx
+++ src/components/d3JS/barChart.tsx
@@ -1,15 +1,58 @@
 import * as d3 from "d3";
 import { Box, Typography } from "@mui/material"
 import { sales } from "../../dummyData/sales";
+import { useEffect, useMemo, useRef } from "react";
+
+
 
 export function BarChart({
   width = 640,
   height = 400,
   marginTop = 20,
-  marginRight = 20,
+  marginLeft = 60,
+  marginRight = 60,
   marginBottom = 20,
 }){
 
+    const gx = useRef();
+    const gy = useRef();
+    const dateScale = [marginLeft, width - marginRight];
+    const ranges = useMemo(()=> {
+        return sales.reduce((acc, item) => {
+            let total = item.price * item.quantity;
+            if(item.discount)
+                total = applyDiscount(total, item.discount);
+            if(acc.maxTotal < total)
+                acc.maxTotal = total;
+            console.log(total)
+            const saleDate = new Date(item.timeStamp);
+            if(acc.start > saleDate)
+                acc.start = saleDate;
+            if(acc.end < saleDate)
+                acc.end = saleDate;
+            return acc;
+        }, {maxTotal: 0, start: new Date(), end: new Date("1900-1-1")});
+    }, []);
+
+    const x = d3.scaleTime(
+        [ranges.start, ranges.end],
+        dateScale
+    );
+
+    const y = d3.scaleLinear(
+        [ranges.maxTotal, 0],
+        [marginTop, height - marginBottom]
+    );
+
+    useEffect(()=>{
+        d3.select(gx.current)
+        .call(d3.axisBottom(x));
+    }, [gx, x])
+
+    useEffect(()=>{
+        d3.select(gy.current)
+        .call(d3.axisLeft(y));
+    }, [gy, y])
 
     return (
         <Box 
@@ -26,8 +69,40 @@ export function BarChart({
                 component={"article"}
             >
                 <svg width={width} height={height}>
+                    <g ref={gx} transform={`translate(0, ${height - marginBottom})`}/>
+                    <g ref={gy} transform={`translate(${marginLeft}, 0)`}/>
+                    <g >
+                        {sales.map((sale, i) => {
+                            let total = sale.quantity * sale.price;
+                            const saleDate = new Date(sale.timeStamp);
+                            if(sale.discount)
+                                total = applyDiscount(total, sale.discount);
+                            console.log(total)
+                            return (<g>
+                                <rect 
+                                    key={"charBars" + i} 
+                                    x={x(saleDate)} 
+                                    y={y(total)} 
+                                    height={y(0) - y(total)} 
+                                    width={10}
+                                />
+                                <text
+                                    key={"charText" + i} 
+                                    x={x(saleDate)} 
+                                    y={y(total)} 
+                                    height={y(0) - y(total)} 
+                                    width={10}
+                                    style={{fontSize: "12px"}}
+                                >$ {total.toFixed(2)}</text>
+                            </g>)
+                        })}
+                    </g>
                 </svg>
             </Box>
         </Box>
     );
+}
+
+export function applyDiscount(amount: number, discount: number){
+    return amount - (amount * (discount / 100));
 }
